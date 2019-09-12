834e45c5e677371faf2707c4bc462be8c7f33d1d
iwongdotcn <iwong.cn@gmail.com>
2019-09-12 14:29:24 +0800

    fix type deduction on join

--- join.hpp
+++ join.hpp
@@ -12,37 +12,38 @@ NAMESPACE_BEGIN
 template <typename char_t, typename traits_t, typename allocator_t, typename container_t>
 std::basic_string<char_t, traits_t, allocator_t>
 join(const container_t& __strings,
-	char_t const& __delimiter) {
-	using string_type = std::basic_string<char_t, traits_t, allocator_t>;
-	string_type __joined;
-	size_t ncount = __strings.size();
-	for (const string_type& s : __strings) {
-		__joined += s;
-		if (--ncount > 0)
-			__joined.push_back(__delimiter);
-	}
-	return __joined;
-}
-
-template <typename char_t, typename traits_t, typename allocator_t, typename container_t>
-std::basic_string<char_t, traits_t, allocator_t>
-join(const container_t& __strings,
-	std::basic_string<char_t, traits_t, allocator_t> const& __delimiter) {
-	using string_type = std::basic_string<char_t, traits_t, allocator_t>;
-	string_type __joined;
-	size_t ncount = __strings.size();
-	for (const string_type& s : __strings) {
-		__joined += s;
-		if (--ncount > 0)
-			__joined += __delimiter;
-	}
-	return __joined;
+	 std::basic_string<char_t, traits_t, allocator_t> const& __delimiter) {
+  using string_type = std::basic_string<char_t, traits_t, allocator_t>;
+  string_type __joined;
+  size_t ncount = __strings.size();
+  for (const string_type& s : __strings) {
+    __joined += s;
+    if (--ncount > 0)
+      __joined += __delimiter;
+  }
+  return __joined;
 }
 
 template <typename char_t, typename container_t>
 std::basic_string<char_t>
-join(const container_t& _strings, char_t const* _delimiter) {
-	return join(_strings, detail::make_safe_string(_delimiter));
+join(const container_t& __strings,
+     char_t const& __delimiter) {
+  using string_type = std::basic_string<char_t>;
+  string_type __joined;
+  size_t ncount = __strings.size();
+  for (const string_type& s : __strings) {
+    __joined += s;
+    if (--ncount > 0)
+      __joined.push_back(__delimiter);
+  }
+  return __joined;
+}
+
+template <typename char_t, typename container_t>
+std::basic_string<char_t>
+join(const container_t& _strings,
+     char_t const* _delimiter) {
+  return join(_strings, detail::make_safe_string(_delimiter));
 }
 
 NAMESPACE_END
